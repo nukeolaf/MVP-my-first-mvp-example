3adeebfaadbdbe137cd001632754de746f26bfac
nukeolaf <yhjin04170@gmail.com>
2020-03-05 00:10:16 +0900

    [Add] MainPresenter 에서 MainContract.Presenter 의 setInfo() override

--- app/src/main/java/com/example/mvp_my_first_mvp_example/MainPresenter.kt
+++ app/src/main/java/com/example/mvp_my_first_mvp_example/MainPresenter.kt
@@ -4,4 +4,8 @@ class MainPresenter : MainContract.Presenter {
     // MainContract.Presenter 에서 정의한 내용을 구현한다
 
     private var view: MainContract.View? = null
+
+    override fun setInfo() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
