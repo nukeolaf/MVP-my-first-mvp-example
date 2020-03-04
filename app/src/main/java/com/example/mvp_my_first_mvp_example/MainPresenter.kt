27e22f798179ab0c18cedc1ed36546b50931c231
nukeolaf <yhjin04170@gmail.com>
2020-03-04 23:53:02 +0900

    [Add] MainPresenter 가 MainContract.Presenter 를 상속받아 구현
    MainContract.View 를 선언한다

--- app/src/main/java/com/example/mvp_my_first_mvp_example/MainPresenter.kt
+++ app/src/main/java/com/example/mvp_my_first_mvp_example/MainPresenter.kt
@@ -1,2 +1,7 @@
 package com.example.mvp_my_first_mvp_example
 
+class MainPresenter : MainContract.Presenter {
+    // MainContract.Presenter 에서 정의한 내용을 구현한다
+
+    private var view: MainContract.View? = null
+}
