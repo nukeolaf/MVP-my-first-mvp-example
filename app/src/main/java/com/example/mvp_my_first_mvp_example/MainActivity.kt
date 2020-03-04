f9e0a1316344da4ee7567b9789f9fa859da71864
nukeolaf <yhjin04170@gmail.com>
2020-03-05 00:19:55 +0900

    [Add] MainActivity 에 presenter 객체 생성

--- app/src/main/java/com/example/mvp_my_first_mvp_example/MainActivity.kt
+++ app/src/main/java/com/example/mvp_my_first_mvp_example/MainActivity.kt
@@ -5,9 +5,13 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity(), MainContract.View{
 
+    lateinit var presenter: MainPresenter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        presenter = MainPresenter()
     }
 
     override fun saveInfo() {
