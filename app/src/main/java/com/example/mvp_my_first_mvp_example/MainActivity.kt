5dce3dd49ace7f990f4db1b1f7ae8801614a22fc
nukeolaf <yhjin04170@gmail.com>
2020-03-04 23:55:37 +0900

    [Add] MainActivity MainContract.View 를 상속

--- app/src/main/java/com/example/mvp_my_first_mvp_example/MainActivity.kt
+++ app/src/main/java/com/example/mvp_my_first_mvp_example/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.mvp_my_first_mvp_example
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainContract.View{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
