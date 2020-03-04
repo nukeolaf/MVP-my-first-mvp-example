dfd75cba14470d68bc1ff2fcf3c3d0ff2e2ebde8
nukeolaf <yhjin04170@gmail.com>
2020-03-05 03:56:59 +0900

    [ADD] shared 에 저장된 info 불러오는 getInfo() 추가

--- app/src/main/java/com/example/mvp_my_first_mvp_example/MainModel.kt
+++ app/src/main/java/com/example/mvp_my_first_mvp_example/MainModel.kt
@@ -8,11 +8,16 @@ class MainModel {
     lateinit var context: Context
 
     fun saveInfo(info: JSONObject) {
-
         var data = info.toString()
 
         var pref = context.getSharedPreferences("info", 0)
         var editor = pref.edit()
         editor.putString("info", data)
+    }
+
+    fun getInfo(): String? {
+        var pref = context.getSharedPreferences("info", 0)
+        var result = pref.getString("info", null)
+        return result
     }
 }
