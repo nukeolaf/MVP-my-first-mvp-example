2099de8c2418f14bd47b7861df2aff96e6e51d5c
nukeolaf <yhjin04170@gmail.com>
2020-03-04 23:49:42 +0900

    [Add] MainContract 에 View 와 Presenter 구현을 위한 준비
    View 와 Presenter 의 interface 를 만들어준다.

--- app/src/main/java/com/example/mvp_my_first_mvp_example/MainContract.kt
+++ app/src/main/java/com/example/mvp_my_first_mvp_example/MainContract.kt
@@ -1,2 +1,11 @@
 package com.example.mvp_my_first_mvp_example
 
+public interface MainContract {
+    interface View {
+        // View method
+    }
+
+    interface Presenter {
+        // Presenter method
+    }
+}
